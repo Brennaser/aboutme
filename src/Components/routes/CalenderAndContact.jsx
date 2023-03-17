27c05320106610b37d3b5df137dd6b34c694f14a
Brennaser <sermanbc@dukes.jmu.edu>
2023-03-17 03:23:42 -0400

    Padding on Calendar

--- src/Components/routes/CalenderAndContact.jsx
+++ src/Components/routes/CalenderAndContact.jsx
@@ -5,7 +5,9 @@ export const CalenderAndContact = () => {
         <div>
             <Navbar />
             <h1>Calender and Contact Info</h1>
-            <iframe src="https://calendar.google.com/calendar/embed?src=spb1m8so2ceskt7jb4a863f95g%40group.calendar.google.com&ctz=America%2FNew_York&src=5has6lsonjrj3kc83acrn0kb0g%40group.calendar.google.com&ctz=America%2FNew_York&src=brennagirl13001%40gmail.com&ctz=America%2FNew_York" title=" " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <div style={"padding-left: 5%"}>
+                <iframe src="https://calendar.google.com/calendar/embed?src=spb1m8so2ceskt7jb4a863f95g%40group.calendar.google.com&ctz=America%2FNew_York&src=5has6lsonjrj3kc83acrn0kb0g%40group.calendar.google.com&ctz=America%2FNew_York&src=brennagirl13001%40gmail.com&ctz=America%2FNew_York" title=" " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            </div>
         </div>    
         );
 }
